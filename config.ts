ad65aad071cf37ec6335b553c3d47a14289dc4ad
xia <xiadd0102@gmail.com>
2022-04-14 14:16:09 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,3 +1,35 @@
 export default {
   title: 'Theirsverse',
+  socials: [
+    {
+      title: 'gmail',
+      img: 'https://theirsverse-resource.s3.ap-northeast-1.amazonaws.com/footer/mail.svg',
+      link: '/'
+    },
+    {
+      title: 'instagram',
+      img: 'https://theirsverse-resource.s3.ap-northeast-1.amazonaws.com/footer/ins.svg',
+      link: '/'
+    },
+    {
+      title: 'tiktok',
+      img: 'https://theirsverse-resource.s3.ap-northeast-1.amazonaws.com/footer/tiktok.svg',
+      link: '/'
+    },
+    {
+      title: 'medium',
+      img: 'https://theirsverse-resource.s3.ap-northeast-1.amazonaws.com/footer/medium.svg',
+      link: '/'
+    },
+    {
+      title: 'opensea',
+      img: 'https://theirsverse-resource.s3.ap-northeast-1.amazonaws.com/footer/opensea.svg',
+      link: '/'
+    },
+    {
+      title: 'twitter',
+      img: 'https://theirsverse-resource.s3.ap-northeast-1.amazonaws.com/footer/twitter.svg',
+      link: '/'
+    },
+  ]
 }
