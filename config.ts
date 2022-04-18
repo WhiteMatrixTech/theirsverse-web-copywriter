b77d0dd0648522eed8d1b97d78de539696efdaae
zy0228 <2472305432@qq.com>
2022-04-18 15:48:25 +0800

    fix: remove no use

--- config.ts
+++ config.ts
@@ -111,16 +111,13 @@ export default {
       {
         image: BaseURL + 'nftcollection/RAINBOW.png',
         title: 'RAINBOW',
-        des: 'Love is love.',
         background: '#CDC3C4',
-        rgb: [205, 195, 196],
         lock: false,
         target: BaseURL + 'rainbow.gif'
       },
       {
         image: BaseURL + 'nftcollection/BINARYOPPOSITIONS-JY.png',
         title: 'BINARY OPPOSITIONS',
-        des: 'The world is not just black or white. ',
         background: '#B8C8D1',
         rgb: [184, 200, 209],
         lock: true
@@ -128,57 +125,43 @@ export default {
       {
         image: BaseURL + 'nftcollection/QUEEN-JY.png',
         title: 'QUEEN',
-        des: 'Gender is not objective, it’s from the mind.',
         background: '#E5CFD3',
-        rgb: [229, 207, 211],
         lock: true
       },
       {
         image: BaseURL + 'nftcollection/SUPER+STAR-JY.png',
         title: 'SUPER STAR',
-        des: 'Shining your light!',
         background: '#E5EAC3',
-        rgb: [229, 234, 195],
         lock: true
       },
       {
         image: BaseURL + 'nftcollection/TAROT-JY.png',
         title: 'TAROT',
-        des: 'Talk to the future.',
         background: '#E3B5D5',
-        rgb: [227, 181, 213],
         lock: true
       },
       {
         image: BaseURL + 'nftcollection/ZODIAC-JY.png',
         title: '12 ZODIAC',
-        des: 'tions or the Eastern zodiac signs, you can use them to find out about your personality and destiny.!',
         background: '#E1DFC8',
-        rgb: [225, 223, 200],
         lock: true
       },
       {
         image: BaseURL + 'nftcollection/SUIT-JY.png',
         title: 'SUIT',
-        des: 'We all have two souls',
         background: '#BCCEC0',
-        rgb: [188, 206, 192],
         lock: true
       },
       {
         image: BaseURL + 'nftcollection/CYBERPUNK.png',
         title: 'CYBERPUNK',
-        des: 'Lowlife and high-tech.',
         background: '#E7CFE3',
-        rgb: [231, 207, 227],
         lock: true
       },
       {
         image: BaseURL + 'nftcollection/DYNASTY-JY.png',
         title: 'DYNASTY',
-        des: 'Love different cultures. ',
         background: '#CBE6E3',
-        rgb: [203, 230, 227],
         lock: true
       },
     ]
