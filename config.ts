245eb1446b0dcb642bf8ad62790df0b1e458a859
zy0228 <2472305432@qq.com>
2022-04-19 17:18:11 +0800

    feat: replace nft img

--- config.ts
+++ config.ts
@@ -22,63 +22,63 @@ export default {
   },
   nft2: [
     {
-      source: BaseURL + 'nftcollection/SUPER+STAR.png',
+      source: BaseURL + 'n-nft1/SUPERSTAR.png',
       title: 'SUPER STAR',
       des: 'Shining your light!',
       background: '#E5EAC3',
       rgb: [229, 234, 195],
     },
     {
-      source: BaseURL + 'nftcollection/RAINBOW.png',
+      source: BaseURL + 'n-nft1/RAINBOW.png',
       title: 'RAINBOW',
       des: 'Love is love.',
       background: '#CDC3C4',
       rgb: [205, 195, 196]
     },
     {
-      source: BaseURL + 'nftcollection/CYBERPUNK.png',
+      source: BaseURL + 'n-nft1/CYBERPUNK.png',
       title: 'CYBERPUNK',
       des: 'Lowlife and high-tech.',
       background: '#E7CFE3',
       rgb: [231, 207, 227]
     },
     {
-      source: BaseURL + 'nftcollection/SUIT-JY.png',
+      source: BaseURL + 'n-nft1/SUIT-JY.png',
       title: 'SUIT',
       des: 'We all have two souls.',
       background: '#BCCEC0',
       rgb: [188, 206, 192]
     },
     {
-      source: BaseURL + 'nftcollection/TAROT-JY.png',
+      source: BaseURL + 'n-nft1/TAROT-JY.png',
       title: 'TAROT',
       des: 'Talk to the future.',
       background: '#E3B5D5',
       rgb: [227, 181, 213]
     },
     {
-      source: BaseURL + 'nftcollection/ZODIAC-JY.png',
+      source: BaseURL + 'n-nft1/ZODIAC-JY.png',
       title: 'ZODIAC',
       des: 'Tions or the Eastern zodiac signs, you can use them to find out about your personality and destiny!',
       background: '#E1DFC8',
       rgb: [225, 223, 200]
     },
     {
-      source: BaseURL + 'nftcollection/QUEEN-JY.png',
+      source: BaseURL + 'n-nft1/QUEEN-JY.png',
       title: 'QUEEN',
       des: 'Gender is not objective, it’s from the mind.',
       background: '#E5CFD3',
       rgb: [229, 207, 211]
     },
     {
-      source: BaseURL + 'nftcollection/BINARYOPPOSITIONS-JY.png',
+      source: BaseURL + 'n-nft1/BINARYOPPOSITION-JY.png',
       title: 'BINARY OPPOSITIONS',
       des: 'The world is not just black or white.',
       background: '#B8C8D1',
       rgb: [184, 200, 209]
     },
     {
-      source: BaseURL + 'nftcollection/DYNASTY-JY.png',
+      source: BaseURL + 'n-nft1/DYNASTY-JY.png',
       title: 'DYNASTY',
       des: 'Love different cultures.',
       background: '#CBE6E3',
@@ -109,58 +109,58 @@ export default {
     },
     box: [
       {
-        image: BaseURL + 'nftcollection/RAINBOW.png',
-        title: 'RAINBOW',
+        image: BaseURL + 'nft1/RAINBOW.png',
+        title: 'Rainbow',
         background: '#CDC3C4',
         lock: false,
         target: BaseURL + 'rainbow.gif'
       },
       {
-        image: BaseURL + 'nftcollection/BINARYOPPOSITIONS-JY.png',
-        title: 'BINARY OPPOSITIONS',
+        image: BaseURL + 'nft1/CYBERPUNK.png',
+        title: 'Cyberpunk',
         background: '#B8C8D1',
         rgb: [184, 200, 209],
         lock: true
       },
       {
-        image: BaseURL + 'nftcollection/QUEEN-JY.png',
-        title: 'QUEEN',
+        image: BaseURL + 'nft1/SUPERSTAR.png',
+        title: 'Super Star',
         background: '#E5CFD3',
         lock: true
       },
       {
-        image: BaseURL + 'nftcollection/SUPER+STAR-JY.png',
-        title: 'SUPER STAR',
+        image: BaseURL + 'nft1/DYNASTY-JY.png',
+        title: 'Dynasty',
         background: '#E5EAC3',
         lock: true
       },
       {
-        image: BaseURL + 'nftcollection/TAROT-JY.png',
-        title: 'TAROT',
+        image: BaseURL + 'nft1/ZODIAC-JY.png',
+        title: '12 Zodiac',
         background: '#E3B5D5',
         lock: true
       },
       {
-        image: BaseURL + 'nftcollection/ZODIAC-JY.png',
-        title: '12 ZODIAC',
+        image: BaseURL + 'nft1/TAROT-JY.png',
+        title: 'Tarot',
         background: '#E1DFC8',
         lock: true
       },
       {
-        image: BaseURL + 'nftcollection/SUIT-JY.png',
+        image: BaseURL + 'nft1/SUIT-JY.png',
         title: 'SUIT',
         background: '#BCCEC0',
         lock: true
       },
       {
-        image: BaseURL + 'nftcollection/CYBERPUNK.png',
-        title: 'CYBERPUNK',
+        image: BaseURL + 'nft1/QUEEN-JY.png',
+        title: 'Queen',
         background: '#E7CFE3',
         lock: true
       },
       {
-        image: BaseURL + 'nftcollection/DYNASTY-JY.png',
-        title: 'DYNASTY',
+        image: BaseURL + 'nft1/BINARYOPPOSITION-JY.png',
+        title: 'Binary Opposition',
         background: '#CBE6E3',
         lock: true
       },
