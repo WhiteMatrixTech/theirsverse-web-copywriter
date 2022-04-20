8efa28fb789af17ee37bc4ef322701b56055d6f0
张瑞 <zrnju@qq.com>
2022-04-20 17:01:10 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -14,7 +14,7 @@ export default {
     line1: 'Theirsverse strives to be an open community',
     line2: ' for building a multi-ethnic,',
     line3: ' social co-creation for art, music, games, and fashion.',
-    part1: 'Each project will donate a percentage of profits to organization that uplift minority at risk programs;',
+    part1: 'Each project will donate a percentage of profits to organizations that uplift minority at risk programs;',
     part2: 'Owners of Theirsverse will have exclusive rewards and updates;',
     part3: 'NFT holders will have priority for art exhibitions and concerts;',
     part4: 'Artists who contributed to our NFT collective will have the right to vote on the selection for limited edition NFTs;',
@@ -53,27 +53,27 @@ export default {
     {
       source: BaseURL + 'n-nft1/TAROT-JY.png',
       title: 'TAROT',
-      des: 'Talk to the future.',
+      des: 'Future premonitions.',
       background: '#E3B5D5',
       rgb: [227, 181, 213]
     },
     {
       source: BaseURL + 'n-nft1/ZODIAC-JY.png',
-      title: 'ZODIAC',
-      des: 'Tions or the Eastern zodiac signs, you can use them to find out about your personality and destiny!',
+      title: 'Zodiac',
+      des: 'Western or Eastern, your soul your destiny.',
       background: '#f7f7dc',
       rgb: [225, 223, 200]
     },
     {
       source: BaseURL + 'n-nft1/QUEEN-JY.png',
-      title: 'QUEEN',
-      des: 'Gender is not objective, it’s from the mind.',
+      title: 'DRAG',
+      des: 'Gender is not construct.',
       background: '#e0b1be',
       rgb: [229, 207, 211]
     },
     {
       source: BaseURL + 'n-nft1/BINARYOPPOSITION-JY.png',
-      title: 'BINARY OPPOSITIONS',
+      title: 'Yin-Yang',
       des: 'The world is not just black or white.',
       background: '#ccd5f6',
       rgb: [184, 200, 209]
@@ -81,7 +81,7 @@ export default {
     {
       source: BaseURL + 'n-nft1/DYNASTY.png',
       title: 'DYNASTY',
-      des: 'Love different cultures.',
+      des: 'Different cultures, same love.',
       background: '#e5d2ba',
       rgb: [203, 230, 227]
     },
@@ -154,19 +154,19 @@ export default {
       },
       {
         image: BaseURL + 'nft1/SUIT-JY.png',
-        title: 'SUIT',
+        title: 'Suit',
         background: '#BCCEC0',
         lock: true
       },
       {
         image: BaseURL + 'nft1/QUEEN-JY.png',
-        title: 'DRAG',
+        title: 'Drag',
         background: '#E7CFE3',
         lock: true
       },
       {
         image: BaseURL + 'nft1/BINARYOPPOSITION-JY.png',
-        title: 'YIN YANG',
+        title: 'Yin-Yang',
         background: '#CBE6E3',
         lock: true
       },
@@ -222,7 +222,7 @@ export default {
     rightDesc1: `Our goal is to celebrate what makes us unique, and more importantly, what unites us.`,
     rightDesc2: `That's why we want to center ourselves around the idea of “theirs”
     a universal term that unifies all creators and their unique stories.`,
-    rightDesc3: `We Are Theirs`
+    rightDesc3: `We Are Theirs!`
   },
   artists: {
     h2Title1: `The Theirsverse eye look was created
